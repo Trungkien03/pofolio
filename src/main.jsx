e129e4f5460774fd4aa4024243f35549bf3e4a53
Trung Kiên Nguyễn <trungkiennguyen0310@gmail.com>
2024-12-14 21:22:43 +0700

    update browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "./common/ThemeContext.jsx";
-import Routers from "./routers/Routers.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+const Routers = React.lazy(() => import("./routers/Routers.jsx"));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <Routers />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter basename="/">
+          <Routers />
+        </BrowserRouter>
+      </Suspense>
     </ThemeProvider>
   </React.StrictMode>
 );
