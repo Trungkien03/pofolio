5be96cfb58d3c1c62fb68c67ff5e6b158bfc557f
Trung Kiên Nguyễn <trungkiennguyen0310@gmail.com>
2024-12-14 21:42:00 +0700

    update loading page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "./common/ThemeContext.jsx";
+import Loading from "./pages/Loading/Loading.jsx";
 
 const Routers = React.lazy(() => import("./routers/Routers.jsx"));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <Routers />
       </Suspense>
     </ThemeProvider>
